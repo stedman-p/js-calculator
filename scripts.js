74461a9fb9e19f7a5cbe60662b6715748b85b471
Peter Stedman <peterfstedman@gmail.com>
2022-08-27 16:46:28 -0400

    wireframe ui work in progress

--- scripts.js
+++ scripts.js
@@ -13,3 +13,18 @@ function multiply(multiplicand, multiplier) {
 function divide(dividend, divisor) {
     return dividend / divisor;
 }
+
+function operate(operator, num1, num2) {
+    switch (operator) {
+        case '+':
+            return add(num1, num2);
+        case '-':
+            return subtract(num1, num2);
+        case '*':
+            return multiply(num1, num2)
+        case '/':
+            return divide(num1, num2)
+        default:
+            break;
+    }
+}
