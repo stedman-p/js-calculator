40396b6713101e5a326924308162c6d98b3170f5
Peter Stedman <peterfstedman@gmail.com>
2022-08-28 19:43:01 -0400

    implement string --> number to evaluate calculator input

--- scripts.js
+++ scripts.js
@@ -1,17 +1,33 @@
 function add(addend1, addend2) {
-    return addend1 + addend2;
+    console.log(`adding ${addend1} to ${addend2}`)
+    return parseInt(addend1) + parseInt(addend2);
 }
 
 function subtract(minuend, subtrahend) {
-    return minuend - subtrahend;
+    console.log(`subtracting ${subtrahend} from ${minuend}`)
+    return parseInt(minuend) - parseInt(subtrahend);
 }
 
 function multiply(multiplicand, multiplier) {
-    return multiplicand * multiplier;
+    console.log(`multiplying ${multiplicand} by ${multiplier}`)
+    return parseInt(multiplicand) * parseInt(multiplier);
 }
 
 function divide(dividend, divisor) {
-    return dividend / divisor;
+    console.log(`dividing ${dividend} by ${divisor}`)
+    return parseInt(dividend) / parseInt(divisor);
+}
+
+function isOperator(value) {
+    switch (value) {
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+            return true;
+        default:
+            return false;
+    }
 }
 
 function operate(operator, num1, num2) {
@@ -29,29 +45,107 @@ function operate(operator, num1, num2) {
     }
 }
 
-function updateDisplay(stringValue, isOperator = false) {
-    if (isOperator) {
+function updateDisplay(stringValue, isCharAnOperator = false) {
+    // don't append an operator to another operator
+    if (isCharAnOperator && isOperator(display.textContent[display.textContent.length - 2])) {
+        return;
+    }
+    if (isCharAnOperator) {
         display.textContent += ' ' + stringValue + ' ';
     }
     else {
-        display.textContent += stringValue;
+        if (display.textContent === '0')
+            display.textContent = stringValue
+        else
+            display.textContent += stringValue;
     }
 }
 
-class Calculation {
-    constructor(leftExpression = '') {
-        this.leftExpression = leftExpression;
-        this.operator = '';
-        this.rightExpression = '';
-    }
-    evaluateExpressions() {
-        if (!isNaN(this.leftExpression) && !isNaN(this.rightExpression)) {
-            operate(this.operator, this.leftExpression, this.rightExpression)
-        }
+function clearDisplay() {
+    display.textContent = '0'
+}
+
+function evaluateDisplay() {
+    if (isOperator(display.textContent[display.textContent.length - 2])) {
+        console.log("don't fuckin do that")
+        return;
     }
 }
 
-let displayString = "5"
+class expressionNode {
+    constructor(data = null, left = null, right = null) {
+        this.data = data;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+function isLeaf(Node) {
+    if (!Node.left | !Node.right)
+        return true;
+    else
+        return false;
+}
+
+rootNode = new expressionNode();
+tststring = "4 + 5 * 3 - 2"
+
+// returns root of tree
+
+function buildExpressionTree(exprString) {
+    // find last occurance of + or - first, else * or /
+    let opIndex = exprString.lastIndexOf('+')
+    if (opIndex === -1)
+        opIndex = exprString.lastIndexOf('-')
+    if (opIndex === -1)
+        opIndex = exprString.lastIndexOf('/')
+    if (opIndex === -1)
+        opIndex = exprString.lastIndexOf('*')
+    if (opIndex === -1) { // no operators = leaf with data = exprString
+        const newNode = new expressionNode(exprString);
+        console.log('found leaf')
+        return newNode;
+    }
+
+    const leftside = exprString.slice(0, opIndex);
+    const rightside = exprString.slice(opIndex + 1);
+
+    console.log('exprstr:"' + exprString + '"')
+    console.log('index:' + opIndex)
+    console.log('operator:' + exprString[opIndex])
+    console.log('leftside:' + leftside)
+    console.log('rightside:' + rightside);
+
+    if (leftside && rightside) {
+        newNode = new expressionNode(exprString[opIndex], buildExpressionTree(leftside), buildExpressionTree(rightside))
+        return newNode;
+    }
+    else if (leftside && !rightside) {
+        newNode = new expressionNode(exprString[opIndex], buildExpressionTree(leftside), null)
+        return newNode;
+    }
+    else if (!leftside && rightside) {
+        newNode = new expressionNode(exprString[opIndex], null, buildExpressionTree(rightside))
+        return newNode;
+    }
+}
+
+function evaluateExpressionTree(exprNode) {
+    // base condition - leaves are numbers
+    if (!isNaN(exprNode.data)) {
+        return exprNode.data;
+    }
+
+    else {
+        return operate(exprNode.data, evaluateExpressionTree(exprNode.left), evaluateExpressionTree(exprNode.right));
+    }
+
+}
+
+theroot = buildExpressionTree(tststring.replace(/\s/g, ''))
+console.log(evaluateExpressionTree(theroot))
+
+let displayString = "0"
 
 const display = document.querySelector('#display')
 
@@ -59,6 +153,8 @@ display.textContent = displayString
 
 const numberButtons = document.querySelectorAll('.calc-button');
 const operatorButtons = document.querySelectorAll('.calc-operator');
+const clearButton = document.querySelector("#clearbtn");
+const evaluateButton = document.querySelector("#evaluatebtn");
 
 numberButtons.forEach(btn => {
     btn.addEventListener("click", () => {
@@ -71,3 +167,7 @@ operatorButtons.forEach(btn => {
         updateDisplay(btn.textContent, true)
     })
 })
+
+clearButton.addEventListener("click", clearDisplay)
+
+evaluateButton.addEventListener("click", evaluateDisplay)
