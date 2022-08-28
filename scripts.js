6ff770cbb31762be349c25b3ac0783ed93fe66d3
Peter Stedman <peterfstedman@gmail.com>
2022-08-28 13:19:36 -0400

    make button press update display

--- scripts.js
+++ scripts.js
@@ -28,3 +28,46 @@ function operate(operator, num1, num2) {
             break;
     }
 }
+
+function updateDisplay(stringValue, isOperator = false) {
+    if (isOperator) {
+        display.textContent += ' ' + stringValue + ' ';
+    }
+    else {
+        display.textContent += stringValue;
+    }
+}
+
+class Calculation {
+    constructor(leftExpression = '') {
+        this.leftExpression = leftExpression;
+        this.operator = '';
+        this.rightExpression = '';
+    }
+    evaluateExpressions() {
+        if (!isNaN(this.leftExpression) && !isNaN(this.rightExpression)) {
+            operate(this.operator, this.leftExpression, this.rightExpression)
+        }
+    }
+}
+
+let displayString = "5"
+
+const display = document.querySelector('#display')
+
+display.textContent = displayString
+
+const numberButtons = document.querySelectorAll('.calc-button');
+const operatorButtons = document.querySelectorAll('.calc-operator');
+
+numberButtons.forEach(btn => {
+    btn.addEventListener("click", () => {
+        updateDisplay(btn.textContent)
+    });
+})
+
+operatorButtons.forEach(btn => {
+    btn.addEventListener("click", () => {
+        updateDisplay(btn.textContent, true)
+    })
+})
